ec0441fbc073915421876b3815ab0fe94cfa3821
totto1230 <joseph.granados@hotmail.com>
2024-03-09 17:40:31 -0600

    first commit

---
+++ Backend/Entidades/Response/ResponseBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForoULAtina.Entidades.Response
+{
+    public class ResponseBase
+    {
+        public Boolean Result { get; set; }
+        public List<String> Errors { get; set; }
+    }
+}
