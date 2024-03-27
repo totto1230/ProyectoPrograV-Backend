6062b35f1060d9a0cefe492eab5624cdb2bfb72b
totto1230 <joseph.granados@hotmail.com>
2024-03-26 21:59:35 -0600

    Min docker

---
+++ Infra/Docker/commands.sh
@@ -0,0 +1,4 @@
+#Tag the image
+docker tag api:dev us-central1-docker.pkg.dev/proyecto-prograv/proyectoimagesapi/apis_proyecto:v1
+#Push the image
+docker push us-central1-docker.pkg.dev/proyecto-prograv/proyectoimagesapi/apis_proyecto:v1
